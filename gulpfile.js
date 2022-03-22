a9ce0acef9e5a1fec7ffa7bec207170b8bbb02ca
vu.tran <vu.tran@spring-production.com>
2022-03-22 17:58:47 +0700

    Gulp task tutorial

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,103 @@
-var gulp = require('gulp');
+var settings = {
+        reload: true,
+        scripts: true
+};
+
+/**
+ * Paths to project folders
+ */
+
+var paths ={
+        input: "src/",
+        output: "dist/",
+      
+        scripts: {
+            input: "src/js/*",
+            polyfills: ".polyfill.js",
+            output: "dist/js/"
+        },
+        html: {
+            input: "src/*.html",
+            output: "dist/"
+        },
+
+
+        reload: "./dist/"
+
+}
+
+
+/**
+ * Gulp Packages
+ */
+
+const { series, watch, src, dest } = require('gulp');
+
+
+// Scripts
+
 var babel = require('gulp-babel');
-var connect = require('gulp-connect');
-var sourcemaps = require('gulp-sourcemaps');
+
+// BrowserSync
+const browserSync = require('browser-sync');
+
+
+/**
+ * Gulp Tasks
+ */
+
+// 
+
+function buildHtmls(){
+        return src(paths.html.input)
+        .pipe(dest(paths.output));
+}
+
+
+// Lint, minify, and concatenate scripts
+var buildScripts = function(done){
+
+    // Make sure this feature is activated before running
+	if (!settings.scripts) return done();
+
+
+}
 
 
 
+function startServer(done){
 
-gulp.task("default", function(){
-    return gulp.src("src/app.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"))
-})
+
+    // Make sure this feature is activated before running.
+    if(!settings.reload) return done();
+
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+
+    done();
+}
+
+
+function reloadBrowser(done) {
+    if(!settings.reload) return done();
+    browserSync.reload();
+    done();
+}
+
+function watchSource(done){
+    watch(paths.input, series(exports.build, reloadBrowser))
+    done();
+}
+
+// Default tasks
+// gulp
+
+exports.default = series(startServer)
+exports.build = series(buildHtmls)
+exports.watch = series(
+    exports.build,
+    startServer,
+    watchSource)
